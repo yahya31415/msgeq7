5dba4c8bd0b335b18b3bf649a2227362222c199b
Yahya Luqman <yahya31415@gmail.com>
2020-02-26 04:23:20 +0300

    single pin 60 leds

--- led_test/led_test.ino
+++ led_test/led_test.ino
@@ -1,69 +1,47 @@
 #include <Adafruit_NeoPixel.h>
 
-#define LEDLOW 8   // input pin Neopixel is attached to
-#define LEDMID 9  
-#define LEDHIG 10 
+#define PIN 8	
 
-#define NUMPIXELS      8 // number of neopixels in strip
+#define NUMPIXELS 60
 
-Adafruit_NeoPixel pixelsLow = Adafruit_NeoPixel(NUMPIXELS, LEDLOW, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel pixelsMid = Adafruit_NeoPixel(NUMPIXELS, LEDMID, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel pixelsHig = Adafruit_NeoPixel(NUMPIXELS, LEDHIG, NEO_GRB + NEO_KHZ800);
+#define UPPER 1000
 
-int delayval = 30; // timing delay in milliseconds
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-int redColor = 0;
-int greenColor = 0;
-int blueColor = 0;
+int delayval = 30;
+float values[7];
+int color[3];
 
 void setup() {
-  // Initialize the NeoPixel library.
-  pixelsLow.begin();
+  pixels.begin();
 }
 
 void loop() {
-  setColor();
-  
-  int low = random(0, NUMPIXELS);
-  int mid = random(0, NUMPIXELS);
-  int high = random(0, NUMPIXELS);
+  setValues();
 
-  for (int i=0; i < (NUMPIXELS-low); i++) {
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixelsLow.setPixelColor(i, pixelsLow.Color(redColor, greenColor, blueColor));
-
-    // This sends the updated pixel color to the hardware.
-    pixelsLow.show();
+  for (int i=0; i < 7; i++) {
+    setColor();
+    float percentage = (values[i]/UPPER);
+    float upf = percentage * (NUMPIXELS/7);
+    int up = upf/1;
+    for(int j=0; j < up; j++){
+      pixels.setPixelColor(((NUMPIXELS/7)*i)+j, pixels.Color(color[0], color[1], color[2]));
+    }
   }
   
-  for (int i=0; i < (NUMPIXELS-mid); i++) {
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixelsMid.setPixelColor(i, pixelsMid.Color(redColor, greenColor, blueColor));
-
-    // This sends the updated pixel color to the hardware.
-    pixelsMid.show();
-  }
-  
-  for (int i=0; i < (NUMPIXELS-high); i++) {
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixelsHig.setPixelColor(i, pixelsHig.Color(redColor, greenColor, blueColor));
-
-    // This sends the updated pixel color to the hardware.
-    pixelsHig.show();
-  }
-  
-  // Delay for a period of time (in milliseconds).
+  pixels.show();
   delay(delayval);
-  
-  pixelsLow.clear();
-  pixelsMid.clear();
-  pixelsHig.clear();
+  pixels.clear();
 }
 
-// setColor()
-// picks random values to set for RGB
 void setColor(){
-  redColor = 255; //random(0, 255);
-  greenColor = 0; //random(0,255);
-  blueColor = 0; //random(0, 255);
+  color[0] = random(0, 255);
+  color[1] = random(0,255);
+  color[2] = random(0, 255);
+}
+
+void setValues() {
+  for(int i=0; i<7; i++) {
+  	values[i] = random(0,UPPER);
+  }
 }
